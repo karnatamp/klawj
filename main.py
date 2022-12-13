119e5505b3a2098bb4e05526ee6221fc54d4b67f
Praveenkumar Reddy Karnatam <75111889+karnatamp@users.noreply.github.com>
2022-12-13 15:11:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import os
+
 from flask import Flask
+
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello():
-    return "Hello from Python!"
+def hello_world():
+    name = os.environ.get("NAME", "World")
+    return "Hello {}!".format(name)
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
